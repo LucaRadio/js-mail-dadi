a315599d69a25738601475d4a5473a3b012b0273
LucaRadio <luca.1407@hotmail.it>
2022-11-03 16:08:37 +0100

    Fix some problems with button

--- js/main.js
+++ js/main.js
@@ -4,27 +4,25 @@ const btnSend = document.querySelector(".btn-primary");
 
 const mails = ["my-mail@gmail.com", "my-mail@yahoo.it", "my-mail@hotmail.it", "my-mail@outlook.it"]
 
-const accountFound = document.querySelector(".container")
-
-let color = "text-danger"
+const accountFound = document.querySelector(".message")
 
 let affermative = false;
 
 btnSend.addEventListener("click", function () {
     mailInserted = mailInput.value;
+    console.log("click");
 
     for (let i = 0; i < mails.length; i++) {
         if (mailInserted === mails[i]) {
             affermative = true;
-            color = "text-success"
         }
     }
     if (affermative === true) {
-        accountFound.innerHTML += `<h1 class="${color} display-5">Your Account is in our database. Congrats!</h1>`
+        accountFound.innerHTML = `Your Account is in our database. Congrats`
         console.log("Your Account is in our database. Congrats!");
     }
     else {
-        accountFound.innerHTML += `<h1 class="${color} display-5">Your Account isn't in our database. Try again!</h1>`
+        accountFound.innerHTML = `Your Account isn't in our database. Try again!`
         console.log("Your Account isn't in our database. Try again!");
     }
 })
